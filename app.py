2bf70485b323d7a5c3b9964a58efe0add1bdd682
hiroto <ff.king.hiroto@gmail.com>
2021-12-28 17:06:31 +0900

    add gitignore, add app.py for gui application

---
+++ app.py
@@ -0,0 +1,16 @@
+import ttkbootstrap as ttk
+from ttkbootstrap.constants import *
+
+
+def main():
+    root = ttk.Window()
+    b1 = ttk.Button(root, text="Button 1", bootstyle=SUCCESS)
+    b1.pack(side=LEFT, padx=5, pady=10)
+    b2 = ttk.Button(root, text="Button 2", bootstyle=(INFO, OUTLINE))
+    b2.pack(side=LEFT, padx=5, pady=10)
+
+    root.mainloop()
+
+
+if __name__ == "__main__":
+    main()
